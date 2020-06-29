77adb3ecba62b9b2661024f324b0d76c29ce5bc2
josh2397 <josh23797@gmail.com>
2020-06-29 22:04:06 +1000

    Fixed up DLS class

--- tiva cpp/DLS.cpp
+++ tiva cpp/DLS.cpp
@@ -1,111 +1,38 @@
 #include "DLS.h"
  
-DLS::DLS(){
- 
+DLS::DLS (Queue *_queue) {
+    queue = _queue;
 }
- 
-void DLS::ReadSerial(char c){
-    char inputString[10] = "";
-    //char c = Helpers::ReadChar();
-    Helpers::WriteChar(c);
-    
-    if (c == '\n'){
+
+void DLS::readSerial(char inChar){
+
+    strcat(DLS::inputString, &inChar);
+
+    if (inChar == '\n') {
+        queue->enqueue(inputString);
         strcpy(inputString, "");
-        //inputString = ;
         return;
     }
-    strcat(inputString, &c);
 }
 
-class DLS {
-private:
-    char CI[4] = "";
-    String inputString = "";
-    char inputChar[30] = "";
+void DLS::eventLoop(){
 
-    Queue *queue;
-    Instruction instruction;
-
-    const int directionPin = 4;
-    const int stepPin = 5;
-
-    // MAX LENGTH - 6600
-    const int StepsPerRev = 6600;
-
-public:
-
-    DLS::DLS (Queue *_queue) {
-        queue = _queue;
-    }
-
-    void DLS::eventLoop(){
-        Serial.println("in");
-        for (;;) {
-            String currentInstruction = queue->peek();
-            if (currentInstruction == "G0") {
-                Serial.println(currentInstruction);
-                spinMotor();
-            }
+    for (;;) {
+        char *currentInstruction = queue->peek();
+        if (currentInstruction == "G0") {
+            // spinMotor();
         }
     }
+}
 
- 
-class DLS {
-private:
-	int inputCharPos = 0;
-    char inputChar[10] = "";
- 
-    Queue *queue;
-    Instruction instruction;
- 
-    const int directionPin = 4;
-    const int stepPin = 5;
- 
-    // MAX LENGTH - 6600
-    const int StepsPerRev = 6600;
- 
-public:
- 
-    DLS::DLS (Queue *_queue) {
-        queue = _queue;
-    }
- 
-    void DLS::eventLoop(){
-        for (;;) {
-            String currentInstruction = queue->peek();
-            if (strcmp(currentInstruction, "G0") == 0) {
-                spinMotor();
-            }else if (){
+// void spinMotor() {
+//     for (int x = 0; x < StepsPerRev; x++) {
+//         digitalWrite(stepPin, HIGH);
+        
+//         delayMicroseconds(800);
+//         digitalWrite(stepPin, LOW);
+        
+//         delayMicroseconds(800);
 
-            }else if (){
-
-            }
-        }
-    }
- 
-    
-    void spinMotor() {
-        for (int x = 0; x < StepsPerRev; x++) {
-            digitalWrite(stepPin, HIGH);
-            
-            delayMicroseconds(800);
-            digitalWrite(stepPin, LOW);
-            
-            delayMicroseconds(800);
- 
-        }
-    }
-    
-    void DLS::readSerial(char inChar){
-    	inputString[inputCharPos] = inChar;
-        inputCharPos++;
-
-        if (inChar == '\n') {
-          	queue->enqueue(inputString);
-
-          	inputCharPos = 0;
-          	break;
-        }
-    }
-
-};
+//     }
+// }
--- tiva cpp/DLS.h
+++ tiva cpp/DLS.h
@@ -1,9 +1,17 @@
 #include <string.h>
 #include "helpers.h"
+#include "Queue.h"
+#include "Instruction.h"
 
 class DLS{
 
+    private:
+        const int StepsPerRev = 6600;
+        char *inputString;
+        Queue *queue;
+        Instruction *instruction;
     public:
-        DLS();
-        void ReadSerial();
+        DLS(Queue *_queue);
+        void eventLoop();
+        void readSerial(char inChar);
 };
